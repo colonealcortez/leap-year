32cc44d90b950002ca4a7dbb9cb39ae907561875
colonealcortez <72023318+colonealcortez@users.noreply.github.com>
2020-09-28 21:26:15 +0530

    leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void is_leap(int x)
+{
+if(x%4==0)||x==0000)
+{
+cout<<"LEAP YEAR :-)"<<endl;
+}
+else
+{
+cout<<"NOT A LEAP YEAR :-("<<endl;
+}
+}
+int main()
+{
+int y;
+cout<<"ENTER YEAR=";
+cin>y;
+cout<<endl;
+is_leap(y);
+return(0);
+}
